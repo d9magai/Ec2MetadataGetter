6f3e45653f20a8530e0cdd65fbb47cfdc85ef2a1
d9magai <kumagai@sencorp.co.jp>
2014-08-17 05:30:51 -0700

    refactor, use sprintf

--- src/Aws/Ec2Metadata.php
+++ src/Aws/Ec2Metadata.php
@@ -66,7 +66,7 @@ class Ec2Metadata
                     'keyname' => $keyname,
                     'index' => $index,
                     'format' => $format,
-                    'key' => $this->get('PublicKeys', $index . '/' . $format)
+                    'key' => $this->get('PublicKeys', sprintf("%s/%s", $index, $format))
             ];
 
             $keys[] = $key;
@@ -101,7 +101,7 @@ class Ec2Metadata
     {
 
         $command = $this->commands[$req];
-        return file_get_contents($this->getFullPath() . $command . '/' . $args);
+        return file_get_contents(sprintf("%s%s/%s", $this->getFullPath(), $command, $args));
     }
 
     /**
