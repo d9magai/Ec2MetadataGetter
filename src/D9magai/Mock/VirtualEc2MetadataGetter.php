1c579f0a8f6f0200c96fddeae3cc8ab9bf034e60
d9magai <kumagai@sencorp.co.jp>
2014-08-20 16:23:13 +0900

    newDirectory isnt need

--- src/D9magai/Mock/VirtualEc2MetadataGetter.php
+++ src/D9magai/Mock/VirtualEc2MetadataGetter.php
@@ -35,8 +35,6 @@ class VirtualEc2MetadataGetter extends \D9magai\Ec2MetadataGetter
     {
 
         $this->vfsRoot = \org\bovigo\vfs\vfsStream::setup(empty($hostname) ? $this->hostname : $hostname);
-        $this->vfsRoot->addChild(\org\bovigo\vfs\vfsStream::newDirectory($this->path));
-
         $file = \org\bovigo\vfs\vfsStream::newFile('latest');
         $file->write('dynamic' . PHP_EOL . 'meta-data' . PHP_EOL .'user-data');
         $this->vfsRoot->addChild($file);
