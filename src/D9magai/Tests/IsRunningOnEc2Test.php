35f70f4c870c77ab8f924a9ef8ede58908cbc4a0
d9magai <kumagai@sencorp.co.jp>
2014-08-20 16:54:54 +0900

    golf

--- src/D9magai/Tests/IsRunningOnEc2Test.php
+++ src/D9magai/Tests/IsRunningOnEc2Test.php
@@ -11,8 +11,7 @@ class IsRunningOnEc2Test extends \PHPUnit_Framework_TestCase
     public function isRunningOnEc2ThrowingRuntimeExceptionTest()
     {
 
-        $ec2metadataGetter = new \D9magai\Mock\VirtualEc2MetadataGetter(\D9magai\Mock\DummyMetadata::$dummyMetadata);
-        $this->assertEquals($ec2metadataGetter->isRunningOnEc2(), true);
+        $this->assertEquals((new \D9magai\Mock\VirtualEc2MetadataGetter(\D9magai\Mock\DummyMetadata::$dummyMetadata))->isRunningOnEc2(), true);
     }
 
 }
